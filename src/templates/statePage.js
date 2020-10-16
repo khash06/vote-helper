962c165348144fb0b4f6dcd57ca1e9a69d916b30
Keishi Hashimoto <khashimoto@tirebuyer.com>
2020-10-15 22:21:41 -0700

    state page styling

--- src/templates/statePage.js
+++ src/templates/statePage.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../utils/fontawesome';
 import Layout from '../components/Layout';
 import Head from '../components/Head';
 import { graphql } from 'gatsby';
@@ -25,19 +28,27 @@ const StatePageTemplate = ({ data }) => {
     return (
         <Layout>
             <Head title={state.state}/>
-            <header>    
-                <h1 className={stateStyles.title}>{state.state}</h1>
-            </header>
-            <section>
-                <h2>Key Dates</h2>
-                <h3>Voter Registration Deadline: {state.keyDates.voterRegistrationDeadline}</h3>
-                <h3>Absentee/Mail-In Application Deadline: {state.keyDates.absenteeRequestDeadline}</h3>
-                <h3>Election Day: November 3rd, 2020</h3>
-            </section>
-            <section>
-                <a href={state.checkRegistrationLink}>Check if you are registered and/or register!</a><br/>
-                <a href={state.absenteeApplicationLink}>Sign up for an absentee/mail-in ballot</a>
-            </section>
+            <Link to="/">Home</Link>
+            <main className={stateStyles.mainContainer}>
+                <header>    
+                    <h1 className={stateStyles.title}>{state.state}</h1>
+                </header>
+                <section className={stateStyles.datesContainer}>
+                    <h2 className={stateStyles.datesTitle}>Key Dates</h2>
+                    <ul>
+                        <li>Voter Registration Deadline: {state.keyDates.voterRegistrationDeadline}</li>
+                        <li>Absentee/Mail-In Application Deadline: {state.keyDates.absenteeRequestDeadline}</li>
+                        <li>Election Day: November 3rd, 2020</li>
+                    </ul>
+                </section>
+                <section className={stateStyles.links}>
+                    <ul>
+                        <li><a href={state.checkRegistrationLink} target="_blank" rel="noopener noreferrer">Check if you are registered and/or register! <FontAwesomeIcon icon={'external-link-alt'}/></a></li>
+                        <li><a href={state.absenteeApplicationLink} target="_blank" rel="noopener noreferrer">Sign up for an absentee/mail-in ballot <FontAwesomeIcon icon={'external-link-alt'}/></a></li>
+                    </ul>
+                    
+                </section>
+            </main>
         </Layout>
     )
 }
